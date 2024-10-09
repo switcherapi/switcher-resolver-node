280a45b85cc206784be15c23111eb97bb962b7ac
Roger Floriano <31597636+petruki@users.noreply.github.com>
2024-10-08 19:45:42 -0700

    Removed deprecated component authorization method - Patched express@5.0.1 (#30)

--- tests/model/component.test.js
+++ tests/model/component.test.js
@@ -1,7 +1,5 @@
 import '../../src/db/mongoose';
 
-import { randomBytes } from 'crypto';
-import bcryptjs from 'bcryptjs';
 import mongoose from 'mongoose';
 import { 
     setupDatabase,
@@ -9,48 +7,15 @@ import {
     domainId,
     domainDocument
  } from '../fixtures/db_api';
-import Component, { EncryptionSalts } from '../../src/models/component';
+import Component from '../../src/models/component';
 
 afterAll(async () => { 
     await new Promise(resolve => setTimeout(resolve, 1000));
     await mongoose.disconnect();
 });
 
-describe('(Deprecated) Testing component authentication', () => {
+describe('Testing component authentication', () => {
     beforeAll(async () => await setupDatabase());
-
-    /**
-     * Generates API key using old method
-     */
-    const generateApiKeyDeprecated = async (component) => {
-        const buffer = randomBytes(32);
-        const apiKey = Buffer.from(buffer).toString('base64');
-        const hash = await bcryptjs.hash(apiKey, EncryptionSalts.COMPONENT);
-        component.apihash = hash;
-        await component.save();
-
-        const generatedApiKey = Buffer.from(apiKey).toString('base64');
-        return generatedApiKey;
-    };
-
-    test('COMPONENT_MODEL - Should authenticate component using old API key format', async () => {
-        // Given
-        const componentId = new mongoose.Types.ObjectId();
-        const component = new Component({
-            _id: componentId,
-            name: 'TestDeprecatedAPIKey',
-            description: 'Test app with depracated API key',
-            domain: domainId,
-            owner: adminMasterAccountId
-        });
-
-        // That
-        const generatedApiKey = await generateApiKeyDeprecated(component);
-        
-        // Test
-        const result = await Component.findByCredentials(domainDocument.name, component.name, generatedApiKey);
-        expect(result.component).not.toBe(undefined);
-    });
 
     test('COMPONENT_MODEL - Should authenticate component using new API key format', async () => {
         // Given
