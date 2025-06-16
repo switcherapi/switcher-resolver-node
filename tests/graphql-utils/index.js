ac2165c8cb70efac0381dea5065460de2b9258c1
Roger Floriano <31597636+petruki@users.noreply.github.com>
2025-06-15 20:47:13 -0700

    Updated GraphQL domain schema to resolve strategy/relay per env (#37)

--- tests/graphql-utils/index.js
+++ tests/graphql-utils/index.js
@@ -12,12 +12,12 @@ export const domainQuery = (where, group, config, strategy) => {
             {
                 domain(${query}) { 
                     name version description activated statusByEnv { env value }
-                    group${elementQuery(group)} { 
+                    group${group !== undefined ? elementQuery(group) : ''} { 
                         name description activated statusByEnv { env value }
-                        config${elementQuery(config)} { 
+                        config${config !== undefined ? elementQuery(config) : ''} { 
                             key description activated statusByEnv { env value }
-                            strategies${elementQuery(strategy)} { 
-                                strategy activated operation values statusByEnv { env value }
+                            strategies${strategy !== undefined ? elementQuery(strategy): ''} { 
+                                strategy activated operation values
                             }
                             relay {
                                 type
@@ -177,6 +177,101 @@ export const expected102 = `
                             "value": false
                         }]
                     }
+                }]
+            }]
+        }
+    }
+}`;
+
+export const expected102Default = `
+{
+    "data": {
+        "domain": {
+            "name": "Domain",
+            "version": 5,
+            "description": "Test Domain",
+            "activated": true,
+            "statusByEnv": [{
+                "env": "default",
+                "value": true
+            }],
+            "group": [{
+                "name": "Group Test",
+                "description": "Test Group",
+                "activated": true,
+                "statusByEnv": [{
+                    "env": "default",
+                    "value": true
+                }],
+                "config": [{
+                    "key": "TEST_CONFIG_KEY",
+                    "description": "Test config 1",
+                    "activated": true,
+                    "statusByEnv": [{
+                        "env": "default",
+                        "value": true
+                    }],
+                    "strategies": [{
+                        "strategy": "VALUE_VALIDATION",
+                        "activated": true,
+                        "operation": "EXIST",
+                        "values": [
+                            "USER_1",
+                            "USER_2",
+                            "USER_3"
+                        ]
+                    }, {
+                        "strategy": "NETWORK_VALIDATION",
+                        "activated": true,
+                        "operation": "EXIST",
+                        "values": [
+                            "10.0.0.0/24"
+                        ]
+                    }, {
+                        "strategy": "TIME_VALIDATION",
+                        "activated": false,
+                        "operation": "BETWEEN",
+                        "values": [
+                            "13:00",
+                            "14:00"
+                        ]
+                    }, {
+                        "strategy": "DATE_VALIDATION",
+                        "activated": false,
+                        "operation": "GREATER",
+                        "values": [
+                            "2019-12-01T13:00"
+                        ]
+                    }],
+                    "relay": {
+                        "type": "NOTIFICATION",
+                        "method": "POST",
+                        "activated": false,
+                        "endpointByEnv": [{
+                            "env": "default",
+                            "value": "http://localhost:3000"
+                        }],
+                        "statusByEnv": [ {
+                            "env": "default",
+                            "value": false
+                        }]
+                    },
+                    "components": ["TestApp"]
+                },
+                {
+                    "key": "TEST_CONFIG_KEY_PRD_QA",
+                    "description": "Test config 2 - Off in PRD and ON in QA",
+                    "activated": false,
+                    "statusByEnv": [{
+                        "env": "default",
+                        "value": false
+                    },{
+                        "env": "QA",
+                        "value": true
+                    }],
+                    "strategies": [],
+                    "relay": null,
+                    "components": []
                 }]
             }]
         }
